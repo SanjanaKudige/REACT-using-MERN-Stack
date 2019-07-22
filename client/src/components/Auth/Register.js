f7267e72feea3d4fcf1513e3688fbf36ad94fc48
Sanjana Kudige Anish Kumar <sanju.ka93@gmail.com>
2019-07-22 15:54:10 -0700

    Designed and added functionality to the Register.js page

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,7 +1,96 @@
 import React, { Component } from 'react'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import { withStyles } from '@material-ui/core/styles'
 
-const Register = () => (
-    <div>this is Register page</div>
-)
+const styles = {
+    TextField: {
+        width: '100%',
+        marginBottom: 5
+    },
+    btnBlock: {
+        textAlign: 'center',
+        marginBottom: 10,
+        marginTop: 20
+    }
+}
 
-export default Register
+class Register extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: '',
+            login: '',
+            password: '',
+            password2: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        const userData = {
+            email: this.state.email,
+            login: this.state.login,
+            password: this.state.password,
+            password2: this.state.password2
+        }
+        console.log(userData)
+    }
+    render() {
+        const { classes } = this.props;
+        return (
+            <Paper style={{ padding: 15 }}>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField
+                        type="email"
+                        label="Email"
+                        className={classes.TextField}
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                    <TextField
+                        label="Login"
+                        type="text"
+                        className={classes.TextField}
+                        name="login"
+                        value={this.state.login}
+                        onChange={this.handleChange}
+                    />
+                    <TextField
+                        label="Password"
+                        type="password"
+                        className={classes.TextField}
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <TextField
+                        label="Repeat Password"
+                        type="password"
+                        className={classes.TextField}
+                        name="password2"
+                        value={this.state.password2}
+                        onChange={this.handleChange}
+                    />
+                    <div className={classes.btnBlock}>
+                        <Button variant="outlined" type="submit"> Submit </Button>
+                    </div>
+                </form>
+            </Paper>
+        )
+    }
+}
+
+
+export default withStyles(styles)(Register)
