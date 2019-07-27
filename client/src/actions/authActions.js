cf57cc888612bcd3c9b869752235c6ae27ebedeb
Sanjana Kudige Anish Kumar <sanju.ka93@gmail.com>
2019-07-27 15:37:58 -0700

    Configured Login page and its validation

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,7 @@
-import { GET_ERRORS } from '../constants'
+import { GET_ERRORS, SET_CURRENT_USER } from '../constants'
 import axios from 'axios'
+import setAuthHeader from '../utils/setAuthHeader'
+
 
 export const registerUser = (userData, history) => dispatch => {
     axios.post('http://localhost:5000/api/users/register', userData)
@@ -8,4 +10,32 @@ export const registerUser = (userData, history) => dispatch => {
             type: GET_ERRORS,
             payload: err.response.data
         }))
+}
+
+export const loginUser = (userData) => dispatch => {
+    axios.post('http://localhost:5000/api/users/login', userData)
+        .then(res => {
+            const { token } = res.data
+            localStorage.setItem('jwtToken', token)
+            setAuthHeader(token)
+            dispatch(getCurrentUser())
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+}
+
+export const getCurrentUser = () => dispatch => {
+    axios.get('http://localhost:5000/api/users')
+        .then(res => dispatch(setCurrentUser(res.data)))
+}
+
+export const setCurrentUser = (data) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: data
+    }
 }
--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,7 +1,100 @@
 import React, { Component } from 'react'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import { withStyles } from '@material-ui/core/styles'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { loginUser } from '../../actions/authActions'
 
-const Login = () => (
-    <div>this is Login page</div>
-)
+const styles = {
+    TextField: {
+        width: '100%',
+        marginBottom: 5
+    },
+    btnBlock: {
+        textAlign: 'center',
+        marginBottom: 10,
+        marginTop: 20
+    }
+}
 
-export default Login
+class Login extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors })
+        }
+    }
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        const userData = {
+            email: this.state.email,
+            password: this.state.password,
+        }
+        // console.log(userData)
+        this.props.loginUser(userData)
+    }
+    render() {
+        const { classes } = this.props;
+        const { errors } = this.state
+        return (
+            <Paper style={{ padding: 15 }}>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField
+                        type="email"
+                        label="Email"
+                        className={classes.TextField}
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        helperText={errors.email ? errors.email : ''}
+                        error={errors.email ? true : false}
+                    />
+
+                    <TextField
+                        label="Password"
+                        type="password"
+                        className={classes.TextField}
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        helperText={errors.password ? errors.password : ''}
+                        error={errors.password ? true : false}
+                    />
+
+                    <div className={classes.btnBlock}>
+                        <Button variant="outlined" type="submit"> Submit </Button>
+                    </div>
+                </form>
+            </Paper>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    errors: state.errors
+})
+
+
+export default connect(mapStateToProps, { loginUser })(withRouter(withStyles(styles)(Login)))
+
+// export default withStyles(styles)(Register)
