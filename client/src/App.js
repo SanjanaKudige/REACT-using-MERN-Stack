9a1b721302dc8af3fb6c2be12ded926928d360c0
Sanjana Kudige Anish Kumar <sanju.ka93@gmail.com>
2019-08-13 17:14:33 -0700

    Added Post.js in server models

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,10 @@ import Main from './components/Layout/Main'
 import Home from './components/Home'
 import Register from './components/Auth/Register'
 import Login from './components/Auth/Login'
+import Profile from './components/Profile/Profile'
+import NotFound from './components/NotFound'
+import Search from './components/Search/NotFound'
+
 import setAuthHeader from './utils/setAuthHeader';
 
 import { logoutUser, getCurrentUser } from './actions/authActions'
@@ -38,6 +42,10 @@ class App extends Component {
                 <Route exact path="/" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/register" component={Register} />
+                <Route path="/profile/:userId" component={Profile} />
+                <Route path="/search" component={Search} />
+                <Route component={NotFound} />
+
               </Switch>
             </Main>
           </BrowserRouter>
